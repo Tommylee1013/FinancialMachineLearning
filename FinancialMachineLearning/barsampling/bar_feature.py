408f5eb64e30260270ea8b1e505013868da338a9
Tommy Lee <junghun1013@icloud.com>
2023-09-09 15:19:00 +0900

    Saving

--- FinancialMachineLearning/barsampling/bar_feature.py
+++ FinancialMachineLearning/barsampling/bar_feature.py
@@ -1,3 +1,6 @@
+import numpy as np
+import pandas as pd
+
 class BarFeature:
     def __init__(self, name, function):
         if not isinstance(name, str):
@@ -8,3 +11,14 @@ class BarFeature:
         self.function = function
     def compute(self, tick_df):
         return self.function(tick_df)
+
+def madOutlier(y, thresh = 3.):
+    median = np.median(y)
+    print(median)
+    diff = np.sum((y - median) ** 2, axis=-1)
+    diff = np.sqrt(diff)
+    print(diff)
+    med_abs_deviation = np.median(diff)
+    modified_z_score = 0.6745 * diff / med_abs_deviation
+    print(modified_z_score)
+    return modified_z_score > thresh
