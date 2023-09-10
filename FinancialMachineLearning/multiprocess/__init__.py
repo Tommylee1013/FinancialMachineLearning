4fdf3f606f14e5d6a44816e260f41a73ef0744f7
Tommy Lee <junghun1013@icloud.com>
2023-09-10 17:55:11 +0900

    Saving

--- FinancialMachineLearning/multiprocess/__init__.py
+++ FinancialMachineLearning/multiprocess/__init__.py
@@ -5,4 +5,4 @@ Utility functions. In particular Chapter20 code on Multiprocessing and Vectoriza
 from FinancialMachineLearning.multiprocess.fast_ewma import ewma
 from FinancialMachineLearning.multiprocess.multiprocess import expand_call, lin_parts, mp_pandas_obj, nested_parts, process_jobs, process_jobs_, \
     report_progress
-from FinancialMachineLearning.multiprocess.utils import get_daily_vol
+from FinancialMachineLearning.multiprocess.utils import getDailyVolatility
