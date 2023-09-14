2cca0aa25348c5391464f7a223915bb0ce63e112
Tommy Lee <101161577+Tommylee1013@users.noreply.github.com>
2023-09-14 20:36:47 +0900

    Saving

--- FinancialMachineLearning/multiprocess/fast_ewma.py
+++ FinancialMachineLearning/multiprocess/fast_ewma.py
@@ -2,7 +2,7 @@ import numpy as np
 from numba import jit
 from numba import float64
 from numba import int64
-@jit((float64[:], int64), nopython=False, nogil=True)
+@jit((float64[:], int64), nopython = False, nogil = True)
 def ewma(arr_in, window):
     arr_length = arr_in.shape[0]
     ewma_arr = np.empty(arr_length, dtype=float64)
