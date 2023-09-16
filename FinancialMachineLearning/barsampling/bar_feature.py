74baa349c077587c64e71b6eddf9ba3b876c291d
Tommy Lee <junghun1013@icloud.com>
2023-09-16 23:27:38 +0900

    Saving

---
+++ FinancialMachineLearning/barsampling/bar_feature.py
@@ -0,0 +1,12 @@
+class BarFeature:
+    def __init__(self, name, function):
+        if not isinstance(name, str):
+            raise ValueError('name must be a string')
+        if not callable(function):
+            raise ValueError('function must be a callable')
+
+        self.name = name
+        self.function = function
+
+    def compute(self, tick_df):
+        return self.function(tick_df)
