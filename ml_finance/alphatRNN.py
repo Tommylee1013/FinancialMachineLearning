fc2226d50c5045d4f48ff01c27b414150a83e3ef
Tommylee1013 <junghun1013@icloud.com>
2023-12-24 12:04:18 +0900

    Saving

---
+++ ml_finance/alphatRNN.py
@@ -0,0 +1,398 @@
+from keras.layers import Layer, RNN
+from keras import backend as K
+from keras import layers
+from keras import *
+from keras.legacy import interfaces
+
+class AlphatRNNCell(Layer):
+    def __init__(self, units,
+                 activation='tanh',
+                 recurrent_activation='sigmoid',
+                 use_bias=True,
+                 kernel_initializer='glorot_uniform',
+                 recurrent_initializer='orthogonal',
+                 bias_initializer='zeros',
+                 kernel_regularizer=None,
+                 recurrent_regularizer=None,
+                 bias_regularizer=None,
+                 kernel_constraint=None,
+                 recurrent_constraint=None,
+                 bias_constraint=None,
+                 dropout=0.,
+                 recurrent_dropout=0.,
+                 implementation=2,
+                 **kwargs):
+        super(AlphatRNNCell, self).__init__(**kwargs)
+        self.units = units
+        self.activation = activations.get(activation)
+        self.recurrent_activation = activations.get(recurrent_activation)
+        self.use_bias = use_bias
+
+        self.kernel_initializer = initializers.get(kernel_initializer)
+        self.recurrent_initializer = initializers.get(recurrent_initializer)
+        self.bias_initializer = initializers.get(bias_initializer)
+
+        self.kernel_regularizer = regularizers.get(kernel_regularizer)
+        self.recurrent_regularizer = regularizers.get(recurrent_regularizer)
+        self.bias_regularizer = regularizers.get(bias_regularizer)
+
+        self.kernel_constraint = constraints.get(kernel_constraint)
+        self.recurrent_constraint = constraints.get(recurrent_constraint)
+        self.bias_constraint = constraints.get(bias_constraint)
+
+        self.dropout = min(1., max(0., dropout))
+        self.recurrent_dropout = min(1., max(0., recurrent_dropout))
+        self.implementation = implementation
+        self.state_size = self.units
+        self.output_size = self.units
+        self._dropout_mask = None
+        self._recurrent_dropout_mask = None
+
+    def build(self, input_shape):
+        input_dim = input_shape[-1]
+
+        if isinstance(self.recurrent_initializer, initializers.Identity):
+            def recurrent_identity(shape, gain=1., dtype=None):
+                del dtype
+                return gain * np.concatenate(
+                    [np.identity(shape[0])] * (shape[1] // shape[0]), axis=1)
+
+            self.recurrent_initializer = recurrent_identity
+
+        self.kernel = self.add_weight(shape=(input_dim, self.units * 2),
+                                      name='kernel',
+                                      initializer=self.kernel_initializer,
+                                      regularizer=self.kernel_regularizer,
+                                      constraint=self.kernel_constraint)
+        self.recurrent_kernel = self.add_weight(
+            shape=(self.units, self.units * 2),
+            name='recurrent_kernel',
+            initializer=self.recurrent_initializer,
+            regularizer=self.recurrent_regularizer,
+            constraint=self.recurrent_constraint)
+
+        if self.use_bias:
+            bias_shape = (2, 2 * self.units)
+            self.bias = self.add_weight(shape=bias_shape,
+                                        name='bias',
+                                        initializer=self.bias_initializer,
+                                        regularizer=self.bias_regularizer,
+                                        constraint=self.bias_constraint)
+            
+            self.input_bias = K.flatten(self.bias[0])
+            self.recurrent_bias = K.flatten(self.bias[1])
+        else:
+            self.bias = None
+
+        # alpha
+        self.kernel_alpha = self.kernel[:, :self.units]
+        self.recurrent_kernel_alpha = self.recurrent_kernel[:, :self.units]
+        # recurrnce
+        self.kernel_h = self.kernel[:, self.units:]
+        self.recurrent_kernel_h = self.recurrent_kernel[:, self.units:]
+
+        if self.use_bias:
+            # bias for inputs
+            self.input_bias_alpha = self.input_bias[:self.units]
+            self.input_bias_h = self.input_bias[self.units:]
+            # bias for hidden state - just for compatibility with CuDNN
+            
+            self.recurrent_bias_alpha = self.recurrent_bias[:self.units]    
+            self.recurrent_bias_h = self.recurrent_bias[self.units:]
+        else:
+            self.input_bias_alpha = None
+            self.input_bias_h = None
+            
+            self.recurrent_bias_alpha = None
+            self.recurrent_bias_h = None
+        self.built = True
+
+    def call(self, inputs, states, training=None):
+        h_tm1 = states[0]  # previous memory
+
+        if 0 < self.dropout < 1 and self._dropout_mask is None:
+            self._dropout_mask = _generate_dropout_mask(
+                K.ones_like(inputs),
+                self.dropout,
+                training=training,
+                count=2)
+        if (0 < self.recurrent_dropout < 1 and
+                self._recurrent_dropout_mask is None):
+            self._recurrent_dropout_mask = _generate_dropout_mask(
+                K.ones_like(h_tm1),
+                self.recurrent_dropout,
+                training=training,
+                count=2)
+
+        # dropout matrices for input units
+        dp_mask = self._dropout_mask
+        # dropout matrices for recurrent units
+        rec_dp_mask = self._recurrent_dropout_mask
+
+        if self.implementation == 1:
+            if 0. < self.dropout < 1.:
+                inputs_alpha = inputs * dp_mask[0]
+                inputs_h = inputs * dp_mask[1]
+            else:
+                inputs_alpha = input
+                inputs_h = inputs
+
+            x_alpha = K.dot(inputs_alpha, self.kernel_alpha)
+            x_h = K.dot(inputs_h, self.kernel_h)
+            if self.use_bias:
+                x_alpha = K.bias_add(x_alpha, self.input_bias_alpha)
+                x_h = K.bias_add(x_h, self.input_bias_h)
+
+            if 0. < self.recurrent_dropout < 1.:
+                h_tm1_alpha = h_tm1 * rec_dp_mask[0]
+                h_tm1_h = h_tm1 * rec_dp_mask[1]
+            else:
+                h_tm1_alpha = h_tm1
+                h_tm1_h = h_tm1
+
+            recurrent_alpha = K.dot(h_tm1_alpha, self.recurrent_kernel_alpha)
+           
+            if self.use_bias:
+                recurrent_alpha = K.bias_add(recurrent_alpha, self.recurrent_bias_alpha)
+
+            alpha = self.recurrent_activation(x_alpha + recurrent_alpha)
+            
+           
+            recurrent_h = K.dot(h_tm1_h, self.recurrent_kernel_h)
+            if self.use_bias:
+                recurrent_h = K.bias_add(recurrent_h, self.recurrent_bias_h)
+            
+            hh = self.activation(x_h + recurrent_h)
+        else:
+            if 0. < self.dropout < 1.:
+                inputs *= dp_mask[0]
+
+            # inputs projected by all gate matrices at once
+            matrix_x = K.dot(inputs, self.kernel)
+            if self.use_bias:
+                # biases: bias_z_i, bias_r_i, bias_h_i
+                matrix_x = K.bias_add(matrix_x, self.input_bias)
+            x_alpha = matrix_x[:, :self.units]
+            x_h = matrix_x[:, self.units: 2 * self.units]
+            
+            if 0. < self.recurrent_dropout < 1.:
+                h_tm1 *= rec_dp_mask[0]
+
+            
+            matrix_inner = K.dot(h_tm1, self.recurrent_kernel)
+            if self.use_bias:
+                  matrix_inner = K.bias_add(matrix_inner, self.recurrent_bias)
+            
+            recurrent_alpha = matrix_inner[:, :self.units] 
+            alpha = self.recurrent_activation(x_alpha + recurrent_alpha)
+            
+            recurrent_h = matrix_inner[:, self.units: 2 * self.units]  
+            hh = self.activation(x_h + recurrent_h)
+
+        # previous and candidate state mixed by update gate
+        h = alpha * h_tm1 + (1 - alpha) * hh
+
+        if 0 < self.dropout + self.recurrent_dropout:
+            if training is None:
+                h._uses_learning_phase = True
+
+        return h, [h]
+
+    def get_config(self):
+        config = {'units': self.units,
+                  'activation': activations.serialize(self.activation),
+                  'recurrent_activation':
+                      activations.serialize(self.recurrent_activation),
+                  'use_bias': self.use_bias,
+                  'kernel_initializer':
+                      initializers.serialize(self.kernel_initializer),
+                  'recurrent_initializer':
+                      initializers.serialize(self.recurrent_initializer),
+                  'bias_initializer': initializers.serialize(self.bias_initializer),
+                  'kernel_regularizer':
+                      regularizers.serialize(self.kernel_regularizer),
+                  'recurrent_regularizer':
+                      regularizers.serialize(self.recurrent_regularizer),
+                  'bias_regularizer': regularizers.serialize(self.bias_regularizer),
+                  'kernel_constraint': constraints.serialize(self.kernel_constraint),
+                  'recurrent_constraint':
+                      constraints.serialize(self.recurrent_constraint),
+                  'bias_constraint': constraints.serialize(self.bias_constraint),
+                  'dropout': self.dropout,
+                  'recurrent_dropout': self.recurrent_dropout,
+                  'implementation': self.implementation}
+        base_config = super(AlphatRNNCell, self).get_config()
+        return dict(list(base_config.items()) + list(config.items()))     
+
+
+class AlphatRNN(RNN):
+    @interfaces.legacy_recurrent_support
+    def __init__(self, units,
+                 activation='tanh',
+                 recurrent_activation='sigmoid',
+                 use_bias=True,
+                 kernel_initializer='glorot_uniform',
+                 recurrent_initializer='orthogonal',
+                 bias_initializer='zeros',
+                 kernel_regularizer=None,
+                 recurrent_regularizer=None,
+                 bias_regularizer=None,
+                 activity_regularizer=None,
+                 kernel_constraint=None,
+                 recurrent_constraint=None,
+                 bias_constraint=None,
+                 dropout=0.,
+                 recurrent_dropout=0.,
+                 implementation=2,
+                 return_sequences=False,
+                 return_state=False,
+                 go_backwards=False,
+                 stateful=False,
+                 unroll=False,
+                 **kwargs):
+        if implementation == 0:
+            warnings.warn('`implementation=0` has been deprecated, '
+                          'and now defaults to `implementation=1`.'
+                          'Please update your layer call.')
+        if K.backend() == 'theano' and (dropout or recurrent_dropout):
+            warnings.warn(
+                'RNN dropout is no longer supported with the Theano backend '
+                'due to technical limitations. '
+                'You can either set `dropout` and `recurrent_dropout` to 0, '
+                'or use the TensorFlow backend.')
+            dropout = 0.
+            recurrent_dropout = 0.
+
+        cell = AlphatRNNCell(units,
+                       activation=activation,
+                       recurrent_activation=recurrent_activation,
+                       use_bias=use_bias,
+                       kernel_initializer=kernel_initializer,
+                       recurrent_initializer=recurrent_initializer,
+                       bias_initializer=bias_initializer,
+                       kernel_regularizer=kernel_regularizer,
+                       recurrent_regularizer=recurrent_regularizer,
+                       bias_regularizer=bias_regularizer,
+                       kernel_constraint=kernel_constraint,
+                       recurrent_constraint=recurrent_constraint,
+                       bias_constraint=bias_constraint,
+                       dropout=dropout,
+                       recurrent_dropout=recurrent_dropout,
+                       implementation=implementation)             
+        super(AlphatRNN, self).__init__(cell,
+                                  return_sequences=return_sequences,
+                                  return_state=return_state,
+                                  go_backwards=go_backwards,
+                                  stateful=stateful,
+                                  unroll=unroll,
+                                  **kwargs)
+        self.activity_regularizer = regularizers.get(activity_regularizer)
+
+    def call(self, inputs, mask=None, training=None, initial_state=None):
+        self.cell._dropout_mask = None
+        self.cell._recurrent_dropout_mask = None
+        return super(AlphatRNN, self).call(inputs,
+                                     mask=mask,
+                                     training=training,
+                                     initial_state=initial_state)
+
+    @property
+    def units(self):
+        return self.cell.units
+
+    @property
+    def activation(self):
+        return self.cell.activation
+
+    @property
+    def recurrent_activation(self):
+        return self.cell.recurrent_activation
+
+    @property
+    def use_bias(self):
+        return self.cell.use_bias
+
+    @property
+    def kernel_initializer(self):
+        return self.cell.kernel_initializer
+
+    @property
+    def recurrent_initializer(self):
+        return self.cell.recurrent_initializer
+
+    @property
+    def bias_initializer(self):
+        return self.cell.bias_initializer
+
+    @property
+    def kernel_regularizer(self):
+        return self.cell.kernel_regularizer
+
+    @property
+    def recurrent_regularizer(self):
+        return self.cell.recurrent_regularizer
+
+    @property
+    def bias_regularizer(self):
+        return self.cell.bias_regularizer
+
+    @property
+    def kernel_constraint(self):
+        return self.cell.kernel_constraint
+
+    @property
+    def recurrent_constraint(self):
+        return self.cell.recurrent_constraint
+
+    @property
+    def bias_constraint(self):
+        return self.cell.bias_constraint
+
+    @property
+    def dropout(self):
+        return self.cell.dropout
+
+    @property
+    def recurrent_dropout(self):
+        return self.cell.recurrent_dropout
+
+    @property
+    def implementation(self):
+        return self.cell.implementation
+
+    def get_config(self):
+        config = {'units': self.units,
+                  'activation': activations.serialize(self.activation),
+                  'recurrent_activation':
+                      activations.serialize(self.recurrent_activation),
+                  'use_bias': self.use_bias,
+                  'kernel_initializer':
+                      initializers.serialize(self.kernel_initializer),
+                  'recurrent_initializer':
+                      initializers.serialize(self.recurrent_initializer),
+                  'bias_initializer': initializers.serialize(self.bias_initializer),
+                  'kernel_regularizer':
+                      regularizers.serialize(self.kernel_regularizer),
+                  'recurrent_regularizer':
+                      regularizers.serialize(self.recurrent_regularizer),
+                  'bias_regularizer': regularizers.serialize(self.bias_regularizer),
+                  'activity_regularizer':
+                      regularizers.serialize(self.activity_regularizer),
+                  'kernel_constraint': constraints.serialize(self.kernel_constraint),
+                  'recurrent_constraint':
+                      constraints.serialize(self.recurrent_constraint),
+                  'bias_constraint': constraints.serialize(self.bias_constraint),
+                  'dropout': self.dropout,
+                  'recurrent_dropout': self.recurrent_dropout,
+                  'implementation': self.implementation}
+        base_config = super(AlphatRNN, self).get_config()
+        del base_config['cell']
+        return dict(list(base_config.items()) + list(config.items()))
+
+    @classmethod
+    def from_config(cls, config):
+        if 'implementation' in config and config['implementation'] == 0:
+            config['implementation'] = 1
+        return cls(**config)
+
+
