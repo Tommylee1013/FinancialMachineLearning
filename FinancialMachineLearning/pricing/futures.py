660f7fcad87ce474adef296b6f7fdc7e8a3959e1
Tommylee1013 <junghun1013@icloud.com>
2024-02-25 23:37:36 +0900

    option pricing update

---
+++ FinancialMachineLearning/pricing/futures.py
@@ -0,0 +1,25 @@
+import numpy as np
+class FutureForwardPricing:
+    def __init__(self, stock_paths, interest_rate, dividend_yield, maturity):
+        self.stock_paths = stock_paths
+        self.interest_rate = interest_rate
+        self.dividend_yield = dividend_yield
+        self.maturity = maturity
+
+    def futures(self):
+        """
+        Calculate the future price as the average of the simulated stock prices at maturity.
+        """
+        future_prices = self.stock_paths[:, -1]  # Prices at maturity
+        future_price = np.mean(future_prices)
+        return future_price
+
+    def forwards(self):
+        """
+        Calculate the forward price using the formula: S0 * exp((r - q) * T),
+        where S0 is the initial stock price, r is the risk-free interest rate,
+        q is the dividend yield, and T is the time to maturity.
+        """
+        S0 = self.stock_paths[0, 0]  # Initial stock price
+        forward_price = S0 * np.exp((self.interest_rate - self.dividend_yield) * self.maturity)
+        return forward_price
--- FinancialMachineLearning/pricing/option.py
+++ FinancialMachineLearning/pricing/option.py
@@ -8,12 +8,90 @@ class OptionPricing:
         self.maturity = maturity
         self.discount_factor = np.exp(-interest_rate * maturity)
 
-    def european_call_option(self, strike_price: float) -> np.ndarray:
-        call_payoffs = np.maximum(self.stock_price_paths - strike_price, 0)
+        self.time_steps = stock_price_paths.shape[-1]
+        self.dt = maturity / (self.time_steps - 1)
+        self.discount_table = np.exp(-interest_rate * np.arange(self.time_steps) * self.dt)
+
+    def european_call_option(self, exercise_price: float) -> np.ndarray:
+        call_payoffs = np.maximum(self.stock_price_paths - exercise_price, 0)
         discounted_call_payoffs = call_payoffs * self.discount_factor
         return discounted_call_payoffs
 
-    def european_put_option(self, strike_price: float) -> np.ndarray:
-        put_payoffs = np.maximum(strike_price - self.stock_price_paths, 0)
+    def european_put_option(self, exercise_price: float) -> np.ndarray:
+        put_payoffs = np.maximum(exercise_price - self.stock_price_paths, 0)
         discounted_put_payoffs = put_payoffs * self.discount_factor
         return discounted_put_payoffs
+
+    def asian_call_option(self, exercise_price: float, average_method: str = 'arithmetic') -> np.ndarray:
+        if average_method == 'arithmetic':
+            average_price = self.stock_price_paths.mean(axis=0)
+        elif average_method == 'geometric':
+            average_price = ss.gmean(self.stock_price_paths, axis=0)
+        else:
+            raise ValueError("avg_method must be either 'arithmetic' or 'geometric'")
+
+        payoffs = np.maximum(average_price - exercise_price, 0)
+        discounted_payoffs = payoffs * self.discount_factor
+
+        return discounted_payoffs
+
+    def asian_put_option(self, exercise_price: float, average_method: str = 'arithmetic') -> np.ndarray:
+        if average_method == 'arithmetic':
+            average_price = self.stock_price_paths.mean(axis=0)
+        elif average_method == 'geometric':
+            average_price = ss.gmean(self.stock_price_paths, axis=0)
+        else:
+            raise ValueError("avg_method must be either 'arithmetic' or 'geometric'")
+
+        payoffs = np.maximum(exercise_price - average_price, 0)
+        discounted_payoffs = payoffs * self.discount_factor
+
+        return discounted_payoffs
+
+    def american_call_option(self, exercise_price: float, poly_degree: int = 2) -> np.ndarray:
+        """
+        American call option pricing using Longstaff-Schwartz method.
+        """
+        payoffs = np.maximum(self.stock_price_paths - exercise_price, 0)
+        cash_flows = np.copy(payoffs)
+
+        for t in reversed(range(1, self.time_steps - 1)):
+            in_the_money = self.stock_price_paths[:, t] > exercise_price
+            if np.any(in_the_money):
+                regression_x = self.stock_price_paths[in_the_money, t]
+                regression_y = cash_flows[in_the_money, t + 1] * np.exp(-self.interest_rate * self.dt)
+                if len(regression_x) > 0:
+                    coefficients = np.polyfit(regression_x, regression_y, poly_degree)
+                    continuation_values = np.polyval(coefficients, self.stock_price_paths[in_the_money, t])
+                    exercise_decision = payoffs[in_the_money, t] > continuation_values
+                    cash_flows[in_the_money, t] = np.where(exercise_decision, payoffs[in_the_money, t],
+                                                           cash_flows[in_the_money, t + 1] * np.exp(
+                                                               -self.interest_rate * self.dt))
+                cash_flows[~in_the_money, t] = cash_flows[~in_the_money, t + 1] * np.exp(-self.interest_rate * self.dt)
+
+        discounted_cash_flows = cash_flows * np.exp(-self.interest_rate * self.dt)
+        return discounted_cash_flows
+
+    def american_put_option(self, exercise_price: float, poly_degree: int = 2) -> np.ndarray:
+        """
+        American put option pricing using longstaff schwartz methods
+        """
+        payoffs = np.maximum(exercise_price - self.stock_price_paths, 0)
+        cash_flows = np.copy(payoffs)
+
+        for t in reversed(range(1, self.time_steps - 1)):
+            in_the_money = self.stock_price_paths[:, t] > exercise_price
+            if np.any(in_the_money):
+                regression_x = self.stock_price_paths[in_the_money, t]
+                regression_y = cash_flows[in_the_money, t + 1] * np.exp(-self.interest_rate * self.dt)
+                if len(regression_x) > 0:
+                    coefficients = np.polyfit(regression_x, regression_y, poly_degree)
+                    continuation_values = np.polyval(coefficients, self.stock_price_paths[in_the_money, t])
+                    exercise_decision = payoffs[in_the_money, t] > continuation_values
+                    cash_flows[in_the_money, t] = np.where(exercise_decision, payoffs[in_the_money, t],
+                                                           cash_flows[in_the_money, t + 1] * np.exp(
+                                                               -self.interest_rate * self.dt))
+                cash_flows[~in_the_money, t] = cash_flows[~in_the_money, t + 1] * np.exp(-self.interest_rate * self.dt)
+
+        discounted_cash_flows = cash_flows * np.exp(-self.interest_rate * self.dt)
+        return discounted_cash_flows
